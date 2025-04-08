42321bbb1cc6675a824ba04520ec5e3ce313cac3
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-08 17:04:37 +0600

    Add landing page layout and component

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,8 @@
+export default function LandingLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        <main>{children}</main>
+      </div>
+    );
+  }
+  
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <p className='text-3xl'>
+            welcome to landing page
+        </p>
+    </div>
+  )
+}
+
+export default LandingPage
