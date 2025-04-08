4326bff20426d0acebe9ed6cb28ffd32d40d7a7d
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-08 21:57:31 +0600

    Add new UI components and authentication layout

---
+++ app/auth/sign-up/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import SignUpForm from "@/feature/auth/sign-up/SignUpForm";
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <Card className="w-[498px] rounded-3xl p-5 md:p-10 flex flex-col gap-10">
+
+                <div className="flex justify-center">
+                    <p>Logo</p>
+                </div>
+
+                <CardHeader className='p-0 space-y-0 flex flex-col gap-2'>
+                    <CardTitle className='text-[28px] font-medium'>Sign up</CardTitle>
+                    <CardDescription className="text-sm font-normal text-label-color">Create a new account</CardDescription>
+                </CardHeader>
+
+                <CardContent className="p-0">
+                    < SignUpForm />
+                </CardContent>
+
+                <CardFooter className="flex-col p-0">
+                    {/* <p className="text-base font-medium">
+                        Already have an account?
+                        {disable ? (
+                            <span className=" text-brand-500 opacity-50 ml-1">
+                                Sign in
+                            </span>
+                        ) : (
+                            <Link
+                                className="text-blue-500 ml-1"
+                                to={"/signin"}
+                            >
+                                Sign in
+                            </Link>
+                        )}
+                    </p> */}
+
+                    <p>Already have an account? Sign In here</p>
+                </CardFooter>
+
+            </Card>
+        </div>
+    )
+}
+
+export default SignUpPage;
---
+++ components/global/InputElement.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { twMerge } from "tailwind-merge";
+import { CircleAlert, Eye, EyeOff } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  labelText?: string;
+  errors?: string;
+  touched?: boolean;
+}
+
+const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
+  (props, ref) => {
+    const { className, labelText, type, errors, touched, ...restProps } = props;
+
+    const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
+
+    const togglePasswordVisibility = () => {
+      setIsPasswordVisible((prevState) => !prevState);
+    };
+
+    return (
+      <div className="w-full flex flex-col justify-center items-start gap-2">
+        <Label className="text-base font-medium">{labelText}</Label>
+
+        <div className="w-full relative">
+          <Input
+            ref={ref}
+            className={twMerge(
+              `text-start ${touched && errors ? " placeholder:text-destructive  border-solid border-destructive !ring-destructive" : ""}`,
+              className
+            )}
+            {...restProps}
+            type={
+              type === "password" && !isPasswordVisible ? "password" : "text"
+            }
+            autoComplete="off"
+          />
+
+          {type === "password" && (
+            <div
+              className="absolute top-1 right-1 cursor-pointer p-2"
+              onClick={togglePasswordVisibility}
+            >
+              {isPasswordVisible ? (
+                <Eye color="#6B7280" size={16} />
+              ) : (
+                <EyeOff color="#6B7280" size={16} />
+              )}
+            </div>
+          )}
+        </div>
+
+        {touched && errors ? (
+          <div className="flex flex-row items-start justify-center gap-2">
+            <div className="flex justify-start mt-[2px]">
+              <CircleAlert size={15} color="#F51714" />
+            </div>
+            <p className="text-danger-500 text-sm font-medium">{errors}</p>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+);
+
+export default InputField;
