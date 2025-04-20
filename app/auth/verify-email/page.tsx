b47bcbbf0693504bbe56f4363656dbe677e9caa3
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-20 18:21:35 +0600

    Add email verification through OTP  page UI

---
+++ app/auth/verify-email/page.tsx
@@ -0,0 +1,31 @@
+import CustomLogo from "@/components/global/CustomLogo";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import VerifyEmailForm from "@/feature/auth/verify-email/VerifyEmailForm";
+
+const VerifyEmailPage = () => {
+    return (
+        <div className="w-[500px] items-center flex flex-col gap-10">
+            <Card className="w-full p-5 md:p-10 rounded-[20px] flex flex-col gap-6 bg-form-color border-none shadow-none">
+
+                <CardHeader className="p-0 space-y-0 flex flex-col gap-6 justify-center items-center">
+
+                    <CustomLogo />
+
+                    <div className="text-center flex flex-col gap-2">
+                        <CardTitle className="text-2xl font-semibold text-primary-font-color">Verify your email</CardTitle>
+                        <CardDescription className="font-normal text-base text-label-color">Enter the one-time password sent to your email to verify</CardDescription>
+                    </div>
+                </CardHeader>
+
+                <CardContent className="p-0">
+                    < VerifyEmailForm />
+                </CardContent>
+
+            </Card>
+
+
+        </div>
+    )
+}
+
+export default VerifyEmailPage;
---
+++ feature/auth/verify-email/VerifyEmailForm.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import CustomButton from '@/components/global/CustomButton'
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp'
+import React from 'react'
+
+const VerifyEmailForm = () => {
+
+    const [value, setValue] = React.useState("")
+
+    return (
+        <form className="flex flex-col gap-6">
+
+            {/* <FormError error={error} /> */}
+
+            <div className="flex flex-col items-start gap-2">
+                <p className="text-base font-medium text-primary-font-color">Enter OTP </p>
+                <div className="w-full flex flex-col gap-5">
+
+                    <InputOTP
+                        maxLength={6}
+                        value={value}
+                        onChange={(value) => setValue(value)}
+                    >
+                        <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                    </InputOTP>
+
+
+                    <CustomButton
+                        type="submit"
+                        label="Verify OTP"
+                        // disabled={isLoading}
+                        // isLoading={isLoading}
+                        className={`rounded-full h-13 text-base font-semibold cursor-pointer ${value.trim() ? "bg-button-primary-color hover:bg-button-primary-color" : "bg-button-disabled-color hover:bg-button-disabled-color cursor-not-allowed"}`}
+                    />
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default VerifyEmailForm
