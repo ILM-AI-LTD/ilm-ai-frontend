fd9e58ae4c163fb1051f4f1d1a55fc298275f503
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-21 17:00:34 +0600

    Update new logo

--- components/global/CustomLogo.tsx
+++ components/global/CustomLogo.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 const CustomLogo = () => {
     return (
         <Image
-          src="/ILM_Logo_Dark.png"
-          width={80}
-          height={53}
+          src="/ILM_AI_Logo.png"
+          width={48}
+          height={60}
           alt="ILM Logo"
         />
       )
