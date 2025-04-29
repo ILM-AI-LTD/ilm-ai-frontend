1db1ac9cdbf55c7ad18f8613409142c26a887a2f
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-29 18:14:27 +0600

    Update landing page components

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,10 @@
 import Footer from '@/components/global/Footer'
-import SmartToolsSection from '@/feature/landing/components/smartTools/SmartToolsSection'
+import CompetitionSection from '@/feature/landing/components/competition/CompetitionSection'
 import FAQSection from '@/feature/landing/components/faq/FAQSection'
 import GettingStartedSection from '@/feature/landing/components/gettingStarted/GettingStartedSection'
 import HeroSection from '@/feature/landing/components/hero/HeroSection'
+import SmartToolsSection from '@/feature/landing/components/smartTools/SmartToolsSection'
+import FeatureDiagram from '@/feature/landing/components/subjectCover/SubjectCoverSection'
 import SubscriptionSection from '@/feature/landing/components/subscription/SubscriptionSection'
 
 const LandingPage = () => {
@@ -10,9 +12,11 @@ const LandingPage = () => {
     <div>
       <HeroSection />
       <SmartToolsSection />
+      <FeatureDiagram />
       <GettingStartedSection />
-      <FAQSection />
       <SubscriptionSection />
+      <FAQSection />
+      <CompetitionSection />
       <Footer />
     </div>
   )
