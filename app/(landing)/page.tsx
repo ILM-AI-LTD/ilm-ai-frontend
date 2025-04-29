54b15ac983f2943c007354d0988f35e0646dac49
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-29 22:32:58 +0600

    Update import statement for SmartToolsSection

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,11 @@
 import Footer from '@/components/global/Footer'
+import SmartToolsSection from '@/feature/landing/components/SmartTools/SmartToolsSection'
 import CompetitionSection from '@/feature/landing/components/competition/CompetitionSection'
 import CustomerService from '@/feature/landing/components/customerService/CustomerService'
 import FAQSection from '@/feature/landing/components/faq/FAQSection'
 import TestimonialMarquee from '@/feature/landing/components/faq/test'
 import GettingStartedSection from '@/feature/landing/components/gettingStarted/GettingStartedSection'
 import HeroSection from '@/feature/landing/components/hero/HeroSection'
-import SmartToolsSection from '@/feature/landing/components/smartTools/SmartToolsSection'
 import FeatureDiagram from '@/feature/landing/components/subjectCover/SubjectCoverSection'
 import SubscriptionSection from '@/feature/landing/components/subscription/SubscriptionSection'
 
