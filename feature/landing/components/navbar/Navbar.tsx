aca2eb6564b81a30d242f05e086079ea930162af
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-04-30 21:57:40 +0600

    Update Navbar styling and add custom breakpoints

--- feature/landing/components/navbar/Navbar.tsx
+++ feature/landing/components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className='h-24 border-b-1 border-border-color flex items-center justify-between bg-primary-bg-color z-10 px-4 md:px-28'>
+        <nav className='h-24 border-b-1 border-border-color flex items-center justify-between bg-primary-bg-color z-10 px-6 md:px-10 2xl:px-[135px]'>
             <div className="flex flex-row gap-2 items-center">
                 <CustomLogo />
                 <h1 className="font-bold text-[32px] text-white ">ILM AI</h1>
