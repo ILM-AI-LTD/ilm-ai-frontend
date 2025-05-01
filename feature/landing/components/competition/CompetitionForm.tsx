842bccaf18381816d3a9359cbbdcf192ee7ec5e0
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-01 17:08:52 +0600

    Refactor competition form and section styles

--- feature/landing/components/competition/CompetitionForm.tsx
+++ feature/landing/components/competition/CompetitionForm.tsx
@@ -4,7 +4,7 @@ import InputField from "@/components/global/CustomInput"
 
 const CompetitionForm = () => {
     return (
-        <form className='w-full lg:w-[65%] p-5 md:p-8 flex flex-col gap-6 border-1 border-border-color rounded-2xl bg-primary-bg-color'>
+        <form className='w-full px-8 py-10 flex flex-col gap-6 border-1 border-border-color rounded-2xl bg-primary-bg-color'>
             <div className="flex flex-col sm:flex-row gap-4">
                 <InputField
                     labelText="First Name"
--- feature/landing/components/competition/CompetitionSection.tsx
+++ feature/landing/components/competition/CompetitionSection.tsx
@@ -2,10 +2,10 @@ import CompetitionForm from "./CompetitionForm"
 
 const CompetitionSection = () => {
     return (
-        <section className='bg-primary-bg-color w-full flex flex-col items-center justify-center px-5 md:px-32 py-32'>
-            <div className='bg-secondary-bg-color rounded-3xl py-16 px-5 md:px-10  text-white flex flex-col gap-8 items-center'>
-                <div className='w-full lg:w-[65%] flex flex-col gap-4 text-center'>
-                    <p className='font-bold text-[min(10vw,48px)]'>Join the ILM Lab Competition and Showcase Your Skills!</p>
+        <section className='bg-primary-bg-color w-full flex flex-col items-center justify-center py-20 px-6 md:px-10 2xl:px-[135px]'>
+            <div className='max-w-[1170px] bg-secondary-bg-color rounded-3xl py-16 p-4 md:p-10 2xl:px-[250px]  text-white flex flex-col gap-8 items-center'>
+                <div className='w-full flex flex-col gap-4 text-center'>
+                    <p className='font-bold text-[min(10vw,36px)]'>Join the ILM Lab Competition and Showcase Your Skills!</p>
                     <p className='font-normal text-lg'>Test your knowledge, get recognized, and win amazing prizes! Ready to put your learning to the test?</p>
                 </div>
 
