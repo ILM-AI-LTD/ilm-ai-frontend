01c7120a61328e8b87b8517467025ba3469449b3
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-01 17:10:02 +0600

    Update Smart Tools section in landing page

--- feature/landing/components/SmartTools/SmartToolsSection.tsx
+++ feature/landing/components/SmartTools/SmartToolsSection.tsx
@@ -3,27 +3,27 @@ import { FeatureCard } from './FeatureCard';
 
 const features = [
   {
-    title: 'ILM Vision',
+    title: 'ILMI Vision',
     description:
-      ' Select lessons from a vast, structured curriculum. Select lessons from a vast, structured curriculum. Select lessons from a vast, structured curriculum.',
+      ' Step into the future of learning with ILMI where personalized education, real connections, and limitless growth begin.',
     imgSrc: 'ilm-vision-icon.svg'
   },
   {
     title: 'Adaptive Tutor',
     description:
-      'Get personalized feedback and recommendations based on your progress and needs. Get personalized feedback and recommendations based on your progress and needs.',
+      'Unlock a learning path that grows with you, guided by your Adaptive Tutor to fit your strengths and challenges.',
     imgSrc: 'adaptive-tutor-icon 1.svg'
   },
   {
     title: 'Interactive Learning',
     description:
-      'Engage with hands‑on exercises and real‑time quizzes for deeper understanding. Engage with hands‑on exercises and real‑time quizzes for deeper understanding.',
+      'Dive into dynamic, hands-on lessons that spark curiosity, build confidence, and keep you excited about learning.',
     imgSrc: 'interactive-learning-flow-icon.svg'
   },
   {
     title: 'ILM Hub',
     description:
-      'Centralized dashboard to track progress, badges, and community interactions.Centralized dashboard to track progress, badges, and community interactions.',
+      'Take charge with the ILM Hub track achievements, access resources, and connect with your learning community, all in one place.',
     imgSrc: 'ilm-hub-icon.svg'
   },
 ];
@@ -32,10 +32,10 @@ const SmartToolsSection: React.FC = () => (
   <section className="bg-secondary-bg-color py-20 px-6 md:px-10 2xl:px-[135px] text-center flex flex-col justify-center items-center">
 
     <h1 className='font-bold text-[min(10vw,36px)] text-white mb-16'>
-      Smart Tools Better Learning
+      Smart Tools, Better Learning
     </h1>
 
-    <div className='max-w-[1170px] grid grid-cols-1 md:grid-cols-2 bg-primary-bg-color rounded-4xl p-4 md:p-6 gap-4 '>
+    <div className='max-w-[1170px] grid grid-cols-1 md:grid-cols-2 bg-primary-bg-color rounded-4xl p-4 md:p-6 2xl:p-20 gap-4 '>
 
       {features.map((feature, idx) => (
         <FeatureCard
