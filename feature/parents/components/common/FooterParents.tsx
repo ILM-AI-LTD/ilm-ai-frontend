9a7624bedd1cae899d89898b6eb41e7ab4674494
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-05 21:44:02 +0600

    Update FooterParents styling

--- feature/parents/components/common/FooterParents.tsx
+++ feature/parents/components/common/FooterParents.tsx
@@ -22,13 +22,13 @@ const FooterParents: React.FC<FooterParentsProps> = ({
     const justifyClass = leftButton ? 'justify-between' : 'justify-end';
 
     return (
-        <div className={`flex items-center border-t-1 border-brand-color-parent ${justifyClass} py-14 bg-transparent ${className}`}>
+        <div className={`flex items-center border-t-1 border-brand-color-parent ${justifyClass} py-6 md:py-14 bg-transparent ${className}`}>
             {leftButton && (
                 <CustomButton
                     label={leftButton.label}
                     onClick={leftButton.onClick}
                     disabled={leftButton.disabled}
-                    className="bg-brand-color-parent text-white py-2 px-6 rounded-full hover:bg-brand-color-parent disabled:opacity-50"
+                    className="bg-brand-color-parent text-white py-2 px-8 rounded-full hover:bg-brand-color-parent disabled:opacity-50"
 
                 />
             )}
@@ -37,7 +37,7 @@ const FooterParents: React.FC<FooterParentsProps> = ({
                 label={rightButton.label}
                 onClick={rightButton.onClick}
                 disabled={rightButton.disabled}
-                className="h-[52px] bg-brand-color-parent text-white p-6 font-bold text-base rounded-full hover:bg-brand-color-parent disabled:opacity-50"
+                className="h-[52px] bg-brand-color-parent text-white py-2 px-8 font-bold text-base rounded-full hover:bg-brand-color-parent disabled:opacity-50"
 
             />
         </div>
