a7a93a8c62241da7f3eff76976d06c4f62966b3e
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-05 17:13:05 +0600

    Refactor layout styling

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
-      <div className="min-h-screen flex justify-center items-center p-5 bg-primary-bg-color">
+      <div className="min-h-screen flex justify-center items-center bg-primary-bg-color">
         {children}
       </div>
     );
