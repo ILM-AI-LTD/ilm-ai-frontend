62e4336444fe07a38a6507fc7acf732a824985c5
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-05 17:13:43 +0600

    Add parent layout and setup page components

---
+++ app/(main)/parents/layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/feature/parents/components/navbar/Navbar";
+
+export default function LandingLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col h-screen w-full bg-primary-bg-color overflow-hidden">
+            <Navbar />
+
+            <main className="flex-1 flex flex-col items-center overflow-hidden px-6 md:px-10 2xl:px-[135px]">
+                {children}
+            </main>
+
+        </div>
+    );
+}
+
---
+++ app/(main)/parents/setup/page.tsx
@@ -0,0 +1,12 @@
+import GetStarted from '@/feature/parents/components/GetStarted'
+import React from 'react'
+
+const page = () => {
+    return (
+        
+            <GetStarted />
+        
+    )
+}
+
+export default page
