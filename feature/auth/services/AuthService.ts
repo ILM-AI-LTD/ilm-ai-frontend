6a403bce33d2cd880602fec1dae3f1cc7d47c434
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-16 16:57:53 +0600

    Refactor AuthService to include separate sign-in methods for parents and students

--- feature/auth/services/AuthService.ts
+++ feature/auth/services/AuthService.ts
@@ -1,19 +1,26 @@
 import { AuthEndpoints } from '@/constants/Endpoints'
-import type { AuthResponse, ResetPasswordDto, ResetPasswordResponse, SetNewPasswordDto, SetNewPasswordResponse, SignInDto, SignUpDto } from '@/types/auth'
+import type { AuthParentsResponse, AuthStudentsResponse, ResetPasswordDto, ResetPasswordResponse, SetNewPasswordDto, SetNewPasswordResponse, SignInParentsDto, SignInStudentsDto, SignUpDto } from '@/types/auth'
 import { apiRequest } from '@/utils/axios'
 
 export const AuthService = {
-    signUp: (data: SignUpDto): Promise<AuthResponse> =>
+    signUp: (data: SignUpDto): Promise<AuthParentsResponse> =>
         apiRequest(AuthEndpoints.signUp, {
             method: 'POST',
             data,
         }),
 
-    signIn: (data: SignInDto): Promise<AuthResponse> =>
-        apiRequest(AuthEndpoints.signIn, {
+    signInParents: (data: SignInParentsDto): Promise<AuthParentsResponse> =>
+        apiRequest(AuthEndpoints.signInParents, {
             method: 'POST',
             data,
         }),
+
+    signInStudents: (data: SignInStudentsDto): Promise<AuthStudentsResponse> =>
+        apiRequest(AuthEndpoints.signInStudents, {
+            method: 'POST',
+            data,
+        }),
+
     resetPassword: (data: ResetPasswordDto): Promise<ResetPasswordResponse> =>
         apiRequest(AuthEndpoints.resetPassword, {
             method: 'POST',
