76937afa706f6db45e8562fef31df3971a9400e4
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-05-19 19:28:21 +0600

    Refactor middleware.ts to handle authentication logic and update route matchers

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,21 @@
+// middleware.ts
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
-import { Protected_Endpoints } from './constants/Endpoints'
 import { getCookie } from './lib/cookies/cookies-server'
 
 export async function middleware(request: NextRequest) {
-    const token = await getCookie('token')
+  const token = await getCookie('token')
 
-    const isProtected = Protected_Endpoints.some(path =>
-        request.nextUrl.pathname.startsWith(path)
-    )
+  if (!token) {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  }
 
-    if (isProtected && !token) {
-        return NextResponse.redirect(new URL('/auth/sign-in', request.url))
-    }
-    return NextResponse.next()
+  return NextResponse.next()
 }
 
 export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
-    ],
+  matcher: [
+    '/parents/:path*',
+    '/student/:path*',
+  ],
 }
