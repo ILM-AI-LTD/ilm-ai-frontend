41a432ad962c1b70f55d4d626db3eb964350b9bd
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-06-05 17:14:16 +0600

    Add physics chapters and types for subchapters and connections

---
+++ feature/students/components/test/types.ts
@@ -0,0 +1,19 @@
+export type SubChapter = {
+    id: string;
+    subChapter: string;
+    href: string;
+    progress: number;
+    icon: string;
+  };
+
+  export type Connection = {
+    from: string;
+    to: string;
+    startYOffset?: number;
+  };
+  
+  
+  export type LayoutItem =
+    | { type: "single"; topicId: string; hidden?: boolean; className?: string; top?: boolean }
+    | { type: "branch"; leftTopicId: string; junctionKey: string };
+  
