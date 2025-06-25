367f19bca879ea402a0b9cc134a9ada4821deaff
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-06-25 20:20:56 +0600

    Replace ILMIAssistant with ILMIAssistantv2 in SuccessfulSetup page

--- app/(main)/parents/setup/successful/page.tsx
+++ app/(main)/parents/setup/successful/page.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
-import ILMIAssistant from "@/feature/parents/components/setup/common/ILMIAssistant";
 import SuccessfulSetup from "@/feature/parents/components/setup/successful/SuccessfulSetup";
+import ILMIAssistantv2 from "@/feature/parents/components/setup/common/ILMIAssistantv2";
 
 export default function Page() {
   return (
     <Suspense fallback={
       <div className="h-full w-full flex items-center justify-center">
         <div className="flex flex-col items-center gap-4">
-          <ILMIAssistant height={180} width={140} className="h-[180px] w-[140px]" />
+          <ILMIAssistantv2 height={180} width={140} className="h-[180px] w-[140px]" />
           <p className="text-white text-lg">Loading data…</p>
         </div>
       </div>
