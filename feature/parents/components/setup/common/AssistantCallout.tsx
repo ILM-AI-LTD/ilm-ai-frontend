379ec511f91f3698ef8b2274fcd3a588e819faea
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-06-25 20:25:41 +0600

    Update AssistantCallout component to use brand color instead of parent chatbox color

--- feature/parents/components/setup/common/AssistantCallout.tsx
+++ feature/parents/components/setup/common/AssistantCallout.tsx
@@ -11,13 +11,12 @@ export interface AssistantCalloutProps {
 const AssistantCallout: React.FC<AssistantCalloutProps> = ({ message, orientation, className = '' }) => {
     return (
         <div className={`relative ${className}`}>
-            <div className="bg-parent-chatbox-color text-white px-6 py-4 text-lg font-normal rounded-xl ">
+            <div className="bg-brand-color text-white px-6 py-4 text-lg font-normal rounded-xl ">
                 {message}
             </div>
             <div
-                className={`absolute transform w-4 h-4 bg-parent-chatbox-color rotate-45  ${orientation === 'bottom' ? "-translate-x-1/2 left-1/2 -bottom-2" : orientation === 'top' ? "-translate-x-1/2 left-1/2 -top-2" : orientation === 'left' ? "-left-2   top-1/2  -translate-y-1/2" : orientation === 'right' ? "-right-2  top-1/2  -translate-y-1/2" : ""}`}
+                className={`absolute transform w-4 h-4 bg-brand-color rotate-45  ${orientation === 'bottom' ? "-translate-x-1/2 left-1/2 -bottom-2" : orientation === 'top' ? "-translate-x-1/2 left-1/2 -top-2" : orientation === 'left' ? "-left-2   top-1/2  -translate-y-1/2" : orientation === 'right' ? "-right-2  top-1/2  -translate-y-1/2" : ""}`}
                 aria-hidden="true">
-
             </div>
 
         </div>
