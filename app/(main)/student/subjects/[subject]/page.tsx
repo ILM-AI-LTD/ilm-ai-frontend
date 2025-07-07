dc699c467ffd60cdeda16d652b7eec5f169662ef
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-07-07 17:29:55 +0600

    Refactor imports and pass subject prop to ChapterList component

--- app/(main)/student/subjects/[subject]/page.tsx
+++ app/(main)/student/subjects/[subject]/page.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ScrollToTopButton from "@/feature/students/subject/components/ScrollToTopButton";
+import { BadgeCard } from "@/feature/students/subject/components/BadgeCard";
 import { ChapterList } from "@/feature/students/subject/components/ChapterList";
 import { NavigationCard } from "@/feature/students/subject/components/NavigationCard";
-import { BadgeCard } from "@/feature/students/subject/components/BadgeCard";
+import ScrollToTopButton from "@/feature/students/subject/components/ScrollToTopButton";
 
 interface PageProps {
   params: Promise<{ subject: string }>;
@@ -16,7 +15,9 @@ const page = async ({ params }: PageProps) => {
     <div>
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="order-2 lg:order-1 w-full lg:w-2/3">
-          <ChapterList />
+
+          <ChapterList subject={subject as "physics" | "chemistry" | "math" | "biology"} />
+
         </div>
         <div className="order-1 lg:order-2 w-full lg:w-1/3">
           <div className="flex flex-col md:flex-row lg:flex-col gap-4 lg:sticky lg:top-30 h-fit justify-center items-center md:items-start">
