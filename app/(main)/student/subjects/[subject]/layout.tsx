20da22480b42d7940092196ac15a5dad274fdf90
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-07-08 16:07:06 +0600

    Update SubjectLayout to wrap children in PaperProvider and adjust padding in layout

--- app/(main)/student/subjects/[subject]/layout.tsx
+++ app/(main)/student/subjects/[subject]/layout.tsx
@@ -16,7 +16,7 @@ const SubjectLayout = ({ children }: { children: React.ReactNode }) => {
   const isSlugPage = /\/subjects\/[^\/]+\/[^\/]+\/[^\/]+$/.test(pathname || "");
 
   if (isSlugPage) {
-    return <>{children}</>; // Skip parent layout for [slug]
+    return <PaperProvider>{children}</PaperProvider>;
   }
 
   return (
@@ -27,7 +27,7 @@ const SubjectLayout = ({ children }: { children: React.ReactNode }) => {
           <div className="sticky top-0 z-50">
             <CustomNavbar role={'student'} />
           </div>
-          <div className="flex flex-1 flex-col bg-background gap-4 p-4">
+          <div className="flex flex-1 flex-col bg-background gap-4 px-10 py-4">
             {children}
           </div>
         </SidebarInset>
