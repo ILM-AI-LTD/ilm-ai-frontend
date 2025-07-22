b46c431f2f8d7a8bd61ed732aa879405192bec6e
ahmedprottoy <rumman.ahmed.prottoy@gmail.com>
2025-07-23 00:28:17 +0600

    update chapter data fro all 4 subjects

--- feature/students/subject/constants/biology.ts
+++ feature/students/subject/constants/biology.ts
@@ -1,58 +1,57 @@
-export const biologyChapters = {
-    paper1: {
-      name: "Paper 1",
-      chapters: [
-        {
-          name: "CELL BIOLOGY",
-          icon: "bio_cells",
-          progress: 0,
-          data: [
-            { id: "1", subChapter: "Cell Structure", progress: 0, icon: "bio_cell_structure" },
-            { id: "2", subChapter: "Cell Division", progress: 0, icon: "bio_cell_division" },
-            { id: "3", subChapter: "Transport in Cells", progress: 0, icon: "bio_transport" },
-            { id: "4", subChapter: "Revision Questions for Chapter 1", progress: 0, icon: "bio_ques" },
-          ],
-        },
-        {
-          name: "ORGANISATION",
-          icon: "bio_organisation",
-          progress: 0,
-          data: [
-            { id: "1", subChapter: "Digestive System and Enzymes", progress: 0, icon: "bio_digestion" },
-            { id: "2", subChapter: "Circulatory System", progress: 0, icon: "bio_circulatory" },
-            { id: "3", subChapter: "Health and Disease", progress: 0, icon: "bio_health" },
-            { id: "4", subChapter: "Revision Questions for Chapter 2", progress: 0, icon: "bio_ques" },
-          ],
-        },
-      ],
-    },
-    paper2: {
-      name: "Paper 2",
-      chapters: [
-        {
-          name: "INHERITANCE, VARIATION AND EVOLUTION",
-          icon: "bio_inheritance",
-          progress: 0,
-          data: [
-            { id: "1", subChapter: "Sexual and Asexual Reproduction", progress: 0, icon: "bio_reproduction" },
-            { id: "2", subChapter: "DNA and the Genome", progress: 0, icon: "bio_dna" },
-            { id: "3", subChapter: "Genetic Inheritance", progress: 0, icon: "bio_genetics" },
-            { id: "4", subChapter: "Selective Breeding and Genetic Engineering", progress: 0, icon: "bio_genetic_eng" },
-            { id: "5", subChapter: "Revision Questions for Chapter 1", progress: 0, icon: "bio_ques" },
-          ],
-        },
-        {
-          name: "ECOLOGY",
-          icon: "bio_ecology",
-          progress: 0,
-          data: [
-            { id: "1", subChapter: "Ecosystems and Biodiversity", progress: 0, icon: "bio_ecosystems" },
-            { id: "2", subChapter: "Cycles in Nature", progress: 0, icon: "bio_cycles" },
-            { id: "3", subChapter: "Human Impact on the Environment", progress: 0, icon: "bio_human_impact" },
-            { id: "4", subChapter: "Revision Questions for Chapter 2", progress: 0, icon: "bio_ques" },
-          ],
-        },
-      ],
-    },
-  };
-  
+export const biologyTopics = {
+  paper1: {
+    name: "Paper 1",
+    topics: [
+      {
+        topic_id: "cell-biology",
+        topic_name: "CELL BIOLOGY",
+        icon: "bio_cells",
+        progress: 0,
+        data: [
+          { subTopic_id: "cell-structure", subTopic_name: "Cell Structure", progress: 0, icon: "bio_cell_structure" },
+          { subTopic_id: "cell-division", subTopic_name: "Cell Division", progress: 0, icon: "bio_cell_division" },
+          { subTopic_id: "transport-in-cells", subTopic_name: "Transport in Cells", progress: 0, icon: "bio_transport" },
+        ],
+      },
+      {
+        topic_id: "organisation",
+        topic_name: "ORGANISATION",
+        icon: "bio_organisation",
+        progress: 0,
+        data: [
+          { subTopic_id: "digestive-system-and-enzymes", subTopic_name: "Digestive System and Enzymes", progress: 0, icon: "bio_digestion" },
+          { subTopic_id: "circulatory-system", subTopic_name: "Circulatory System", progress: 0, icon: "bio_circulatory" },
+          { subTopic_id: "health-and-disease", subTopic_name: "Health and Disease", progress: 0, icon: "bio_health" },
+        ],
+      },
+    ],
+  },
+  paper2: {
+    name: "Paper 2",
+    topics: [
+      {
+        topic_id: "inheritance-variation-and-evolution",
+        topic_name: "INHERITANCE, VARIATION AND EVOLUTION",
+        icon: "bio_inheritance",
+        progress: 0,
+        data: [
+          { subTopic_id: "sexual-and-asexual-reproduction", subTopic_name: "Sexual and Asexual Reproduction", progress: 0, icon: "bio_reproduction" },
+          { subTopic_id: "dna-and-the-genome", subTopic_name: "DNA and the Genome", progress: 0, icon: "bio_dna" },
+          { subTopic_id: "genetic-inheritance", subTopic_name: "Genetic Inheritance", progress: 0, icon: "bio_genetics" },
+          { subTopic_id: "selective-breeding-and-genetic-engineering", subTopic_name: "Selective Breeding and Genetic Engineering", progress: 0, icon: "bio_genetic_eng" },
+        ],
+      },
+      {
+        topic_id: "ecology",
+        topic_name: "ECOLOGY",
+        icon: "bio_ecology",
+        progress: 0,
+        data: [
+          { subTopic_id: "ecosystems-and-biodiversity", subTopic_name: "Ecosystems and Biodiversity", progress: 0, icon: "bio_ecosystems" },
+          { subTopic_id: "cycles-in-nature", subTopic_name: "Cycles in Nature", progress: 0, icon: "bio_cycles" },
+          { subTopic_id: "human-impact-on-the-environment", subTopic_name: "Human Impact on the Environment", progress: 0, icon: "bio_human_impact" },
+        ],
+      },
+    ],
+  },
+};
