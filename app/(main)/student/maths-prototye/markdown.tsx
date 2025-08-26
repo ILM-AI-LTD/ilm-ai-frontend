69ec8e4a0829ec318badf6e0999a629cd4db623c
shuvo-99 <anontoshuvo99@gmail.com>
2025-08-26 23:26:03 +0600

    error fixed

--- app/(main)/student/maths-prototye/markdown.tsx
+++ app/(main)/student/maths-prototye/markdown.tsx
@@ -1,47 +1,47 @@
 // components/MathRenderer.js
-"use client";
-import { useEffect, useRef } from "react";
+// "use client";
+// import { useEffect, useRef } from "react";
 
 type Props = {
   content: string;
 };
 
 const MathRenderer = ({ content }: Props) => {
-  const containerRef = useRef();
+  // const containerRef = useRef();
 
-  useEffect(() => {
-    const renderMath = async () => {
-      const katex = (await import("katex")).default;
+  // useEffect(() => {
+  //   const renderMath = async () => {
+  //     const katex = (await import("katex")).default;
 
-      if (containerRef.current) {
-        const parsedContent = content
-          .replace(/\$\$(.*?)\$\$/gs, (match, equation) => {
-            try {
-              return `<div class="math-block">${katex.renderToString(
-                equation.trim()
-              )}</div>`;
-            } catch (e) {
-              return `<div class="math-error">${equation}</div>`;
-            }
-          })
-          .replace(/\$(.*?)\$/g, (match, equation) => {
-            try {
-              return `<span class="math-inline">${katex.renderToString(
-                equation.trim()
-              )}</span>`;
-            } catch (e) {
-              return `<span class="math-error">${equation}</span>`;
-            }
-          });
+  //     if (containerRef.current) {
+  //       const parsedContent = content
+  //         .replace(/\$\$(.*?)\$\$/gs, (match, equation) => {
+  //           try {
+  //             return `<div class="math-block">${katex.renderToString(
+  //               equation.trim()
+  //             )}</div>`;
+  //           } catch (e) {
+  //             return `<div class="math-error">${equation}</div>`;
+  //           }
+  //         })
+  //         .replace(/\$(.*?)\$/g, (match, equation) => {
+  //           try {
+  //             return `<span class="math-inline">${katex.renderToString(
+  //               equation.trim()
+  //             )}</span>`;
+  //           } catch (e) {
+  //             return `<span class="math-error">${equation}</span>`;
+  //           }
+  //         });
 
-        containerRef.current.innerHTML = parsedContent;
-      }
-    };
+  //       containerRef.current.innerHTML = parsedContent;
+  //     }
+  //   };
 
-    renderMath();
-  }, [content]);
+  //   renderMath();
+  // }, [content]);
 
-  return <div ref={containerRef} />;
+  // return <div ref={containerRef} />;
 };
 
 export default MathRenderer;
